54c03505d57df1d68cb5b063ed2a5c9afe719876
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-24 00:30:22 +0300

    fix bug

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,10 +3,9 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('build'))
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
