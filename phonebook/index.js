6f920d412bed8f0744854b002144142396cbbd97
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-23 16:36:55 +0300

    Allow getting of one person from server

--- phonebook/index.js
+++ phonebook/index.js
@@ -33,5 +33,16 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people<br/>${new Date(Date.now()).toString()}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(400).end('Person not found')
+  }
+  
+})
+
 const PORT = 3001
 app.listen(PORT, () => {})
