c47a79d8ba01e1122608922bb0d967503466e155
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-24 01:09:31 +0300

    update static files

--- phonebook/index.js
+++ phonebook/index.js
@@ -36,10 +36,6 @@ let persons =
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello pople</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 })
