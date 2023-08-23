43259d3955816ce4fbecff3cfe6820dd451f7497
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-23 17:06:20 +0300

    Allow adding of person to phonebook and error handling

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = 
 [
   { 
@@ -41,7 +43,29 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(400).end('Person not found')
   }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
   
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
+    })
+  } else if(persons.find(p => p.name === body.name)) {
+    return response.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.round(Math.random()*100)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
