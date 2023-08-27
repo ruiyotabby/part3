00f37f220eeff6c75a5e1b53e1f687de0e977b91
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-27 10:38:01 +0300

    Allow getting of a single person in phonebook

--- phonebook/index.js
+++ phonebook/index.js
@@ -15,29 +15,6 @@ morgan.token('body', req => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// let persons = 
-// [
-//   { 
-//     "id": 1,
-//     "name": "Arto Hellas", 
-//     "number": "040-123456"
-//   },
-//   { 
-//     "id": 2,
-//     "name": "Ada Lovelace", 
-//     "number": "39-44-5323523"
-//   },
-//   { 
-//     "id": 3,
-//     "name": "Dan Abramov", 
-//     "number": "12-43-234345"
-//   },
-//   { 
-//     "id": 4,
-//     "name": "Mary Poppendieck", 
-//     "number": "39-23-6423122"
-//   }
-// ]
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -49,16 +26,12 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people<br/>${new Date(Date.now()).toString()}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(request.params);
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(400).end('Person not found')
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+
+  const person = Person.findById(id).then(returnedPerson => {
+    response.json(returnedPerson)
+  }).catch(err => next(err))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -75,12 +48,7 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({
       error: 'Name or number missing'
     })
-  } 
-  // else if(persons.find(p => p.name === body.name)) {
-  //   return response.status(400).json({
-  //     error: 'Name must be unique'
-  //   })
-  // }
+  }
 
   const person = new Person ({
     name: body.name,
